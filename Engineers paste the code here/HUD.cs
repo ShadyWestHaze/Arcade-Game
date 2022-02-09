f86f4318c9848466121e63a0cc050f16d3d2e5a9
ShadyWestHaze <99255149+ShadyWestHaze@users.noreply.github.com>
2022-02-09 15:10:51 +0100

    Add files via upload

---
+++ Engineers paste the code here/HUD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GXPEngine;
+using System.Drawing;
+
+public class HUD : Canvas
+{
+    
+    private Player _player;
+
+    public HUD(Player player) : base(500,300)
+    {
+        _player = player;
+        
+    }
+    void Update()
+    {
+        this.scale = 2;
+        graphics.Clear(Color.Empty);
+        graphics.DrawString("SCORE:" + _player.GetScore(), SystemFonts.StatusFont, Brushes.White, 0, 10);
+    }
+}
