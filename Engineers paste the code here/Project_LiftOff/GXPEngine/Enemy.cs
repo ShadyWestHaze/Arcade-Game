60a45de5d720ec5a01ec17b4b191d129df8ef1f0
ShadyWestHaze <99255149+ShadyWestHaze@users.noreply.github.com>
2022-02-09 10:28:03 +0100

    Add files via upload
    Classes for enemy / player/ tiles / wall

---
+++ Engineers paste the code here/Project_LiftOff/GXPEngine/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GXPEngine;
+
+public class Enemy : Sprite
+{
+    public Enemy() : base("circle.png")
+    {
+
+    }
+
+    public void EnemyGone()
+    {
+        this.LateDestroy();
+    }
+}
